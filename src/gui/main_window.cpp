db275e6cac61b691072080950dacaa290854b511
Oleksandr Tovshchyk <157808651+takiido@users.noreply.github.com>
2024-10-22 02:14:46 -0500

    Changed GTK to GTKMM; Created main window

---
+++ src/gui/main_window.cpp
@@ -0,0 +1,7 @@
+#include "gui/main_window.hpp"
+
+MainWindow::MainWindow()
+{
+  set_title("Booze");
+  set_default_size(200, 200);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,16 @@
 #include "core/package_manager.hpp"
 #include <memory>
+#include "gui/main_window.hpp"
 
-int main() {
-    auto pm = PackageManager(std::make_shared<System>());
+int main(int argc, char **argv) {
+    // auto pm = PackageManager(std::make_shared<System>());
 
-    pm.removePackage("git");
-    pm.installPackage("git");
-    pm.upgradePackage("git");
-    pm.listInstalledPackages();
+    // pm.removePackage("git");
+    // pm.installPackage("git");
+    // pm.upgradePackage("git");
+    // pm.listInstalledPackages();
+    //
+    auto app = Gtk::Application::create("org.gtkmm.examples.base");
 
-    return 0;
+    return app->make_window_and_run<MainWindow>(argc, argv);
 }
