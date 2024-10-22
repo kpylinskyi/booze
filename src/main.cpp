86a4487185fb8aafebe8c98c38c974557a316782
Oleksandr Tovshchyk <157808651+takiido@users.noreply.github.com>
2024-10-22 02:16:06 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,13 @@
 #include "gui/main_window.hpp"
 
 int main(int argc, char **argv) {
-    // auto pm = PackageManager(std::make_shared<System>());
+    auto pm = PackageManager(std::make_shared<System>());
 
-    // pm.removePackage("git");
-    // pm.installPackage("git");
-    // pm.upgradePackage("git");
-    // pm.listInstalledPackages();
-    //
+    pm.removePackage("git");
+    pm.installPackage("git");
+    pm.upgradePackage("git");
+    pm.listInstalledPackages();
+
     auto app = Gtk::Application::create("org.gtkmm.examples.base");
 
     return app->make_window_and_run<MainWindow>(argc, argv);
