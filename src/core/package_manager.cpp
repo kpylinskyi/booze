8fce0472e151c0a1f50d597205e23bed5a1b05f1
Kyrylo Pylinskyi <k.pylinskyi@gmail.com>
2024-10-23 15:00:33 +0200

    refactor: core

--- src/core/package_manager.cpp
+++ src/core/package_manager.cpp
@@ -2,6 +2,10 @@
 
 PackageManager::PackageManager(std::shared_ptr<System> system) : system(system) {}
 
+bool PackageManager::isPackageInstalled(const std::string& package_name){
+    return system->isPackageInstalled(package_name);
+}
+
 bool PackageManager::installPackage(const std::string& package_name) {
     return system->installPackage(package_name);
 }
@@ -10,26 +14,33 @@ bool PackageManager::upgradePackage(const std::string& package_name) {
     return system->upgradePackage(package_name);
 }
 
-bool PackageManager::removePackage(const std::string& package_name) {
-    return system->removePackage(package_name);
+bool PackageManager::uninstallPackage(const std::string& package_name) {
+    return system->uninstallPackage(package_name);
 }
 
 std::vector<Package> PackageManager::searchPackages(const std::string& query) {
     std::vector<Package> packages;
-    std::vector<std::string> system_output = system->searchPackages(query);
-    // Parse system_output and populate packages as needed
+    std::string system_output;
+    if (system->searchPackages(query, system_output)) {
+        // Parse system_output and populate packages as needed
+    }
     return packages;
 }
 
 std::vector<Package> PackageManager::listInstalledPackages() {
     std::vector<Package> packages;
-    std::vector<std::string> system_output = system->listInstalledPackages();
-    // Parse system_output and populate packages as needed
+    std::string system_output;
+    if (system->listInstalledPackages(system_output)) {
+        // Parse system_output and populate packages as needed
+    }
     return packages;
 }
 
-PackageStatus PackageManager::getPackageInfo(const std::string& package_name) {
-    std::string system_output = system->getPackageInfo(package_name);
-    // Process system_output and return the correct PackageStatus
-    return PackageStatus::NOT_FOUND;  // Example return value
+Package PackageManager::getPackageInfo(const std::string& package_name) {
+    std::string system_output;
+
+    if (system->getPackageInfo(package_name, system_output)){
+        // Parse system_output and populate package
+    }
+    return Package();
 }
--- src/core/system.cpp
+++ src/core/system.cpp
@@ -1,230 +1,102 @@
 #include "core/system.hpp"
 #include <boost/process.hpp>
 #include <boost/process/error.hpp>
-#include <nlohmann/json.hpp>
-#include <sys/stat.h> 
-#include <fstream>
 #include <iostream>
 #include <sstream>
 
-using json = nlohmann::json;
 namespace bp = boost::process;
 
-System::System() 
-    : configDirectory(std::string(getenv("HOME")) + "/.config/booze"),
-      configFileName("system.json"),
-      configFilePath(configDirectory + '/' + configFileName) 
+System::System() {}
+
+bool System::installPackage(const std::string &package_name)
 {
-    initializeConfig();
+    return executeAndHandle("brew install " + package_name);
 }
 
-
-bool System::fileExists(const std::string &path) {
-    std::ifstream file(path);
-    return file.good();
-}
-bool System::createConfigDirectory(const std::string &path){
-    struct stat info;
-
-    if (stat(path.c_str(), &info) != 0){
-        if(mkdir(path.c_str(), 0755) == -1)  {
-            std::cerr << "Error: Could not create directory " << path << std::endl;
-            return false;
-        }
-    } else if (!(info.st_mode & S_IFDIR)) {
-        std::cerr << "Error: Path exists but is not a directory: " << path << std::endl;
-        return false;
-    }
-
-    return true;
+bool System::upgradePackage(const std::string &package_name)
+{
+    return executeAndHandle("brew upgrade " + package_name);
 }
 
-bool System::initializeConfig() {
-    if(!createConfigDirectory(configDirectory)) return false;
-
-    if(!fileExists(configFilePath)) {
-        std::cout << "Config file not found, creating default config..." << std::endl;
-        if (!populateConfig(configFilePath)) {
-            std::cerr << "Error: Failed to create default config." << std::endl;
-            return false;
-        }
-    } else {
-        std::cout << "Config file found, using existing configuration." << std::endl;
-    }
-
-    if(!readConfig(configFilePath)) {
-        std::cerr <<  "Error: Could not read file " << configFilePath << std::endl;
-        return false;
-    }
-
-    return true;
+bool System::uninstallPackage(const std::string &package_name)
+{
+    return executeAndHandle("brew uninstall " + package_name);
 }
 
-bool System::populateConfig(const std::string &path) {
-    json defaultConfig = {
-        {"package_manager", "brew"},
-        {"managers", {
-            {"brew", {
-                {"update", "brew update"},
-                {"install", "brew install"},
-                {"upgrade", "brew upgrade"},
-                {"remove", "brew uninstall"},
-                {"search", "brew search"},
-                {"list", "brew list"},
-                {"info", "brew info"}
-            }},
-        }}
-    };
-
-    std::ofstream configFile(path);
-    if (configFile.is_open()) {
-        configFile << defaultConfig.dump(4); 
-        configFile.close();
-        return true;
-    } else {
-        std::cerr << "Error: Could not write default config to " << path << std::endl;
-        return false;
-    }
+bool System::isPackageInstalled(const std::string &package_name)
+{
+    std::string results;
+    bool success = executeAndCapture("brew list | grep " + package_name, results);
+    
+    return success && results.find(package_name) != std::string::npos ? true : false;
 }
 
-
-bool System::readConfig(const std::string &path) {
-    std::ifstream config_file(path);
-    if (!config_file.is_open()) {
-        std::cerr << "Error: Unable to open config file at " << path << std::endl;
-        return false;
-    }
-
-    json config_json;
-    try {
-        config_file >> config_json;
-    } catch (const json::parse_error &err) {
-        std::cerr << "Error: Failed to parse JSON config file: " << err.what() << std::endl;
-        return false;
-    }
-
-    if (config_json.contains("package_manager")) {
-        activePackageManager = config_json["package_manager"].get<std::string>();
-    } else {
-        std::cerr << "Error: 'package_manager' not specified in config" << std::endl;
-        return false;
-    }
-
-    if (config_json.contains("managers")) {
-        auto managers = config_json["managers"];
-        for (auto& [manager_name, commands] : managers.items()) {
-            std::map<std::string, std::string> command_map;
-            for (auto& [command_name, command_value] : commands.items()) {
-                command_map[command_name] = command_value.get<std::string>();
-            }
-            packageManagers[manager_name] = command_map;
-        }
-    } else {
-        std::cerr << "Error: 'managers' section not found in config" << std::endl;
-        return false;
-    }
-
-    return true;
+bool System::searchPackages(const std::string &query, std::string& results)
+{
+    return executeAndCapture("brew search " + query, results);
 }
 
-bool System::isPackageInstalled(const std::string &package_name) { 
-    std::string isInstalledCommand = packageManagers[activePackageManager]["list"] + " | grep " + package_name;
-    CommandResult result = execute(isInstalledCommand);
-
-    return false;
+bool System::listInstalledPackages(std::string& results)
+{
+    return executeAndCapture("brew list", results);
 }
 
-bool System::installPackage(const std::string &package_name) {
-    std::string installCommand = packageManagers[activePackageManager]["install"] + " " + package_name;
-    CommandResult result = execute(installCommand);
-
-    if (result.exit_code == 0) {
-        std::cout << result.stdout_str << "Package installed successfully.\n";
-        return true;
-    } else {
-        std::cerr << "Failed to install package:\n" << result.stderr_str;
-        return false;
-    }
+bool System::getPackageInfo(const std::string &package_name, std::string& results)
+{
+    return executeAndCapture("brew info " + package_name, results);
 }
 
-bool System::upgradePackage(const std::string &package_name) {
-    std::string updateCommand = packageManagers[activePackageManager]["upgrade"] + " " + package_name;
-    CommandResult result = execute(updateCommand);
-
-    if (result.exit_code == 0) {
-        std::cout << result.stdout_str << "Package updated successfully.\n";
-        return true;
-    } else {
-        std::cerr << "Failed to update package:\n" << result.stderr_str;
-        return false;
-    }
-}
-
-bool System::removePackage(const std::string &package_name) {
-    std::string removeCommand = packageManagers[activePackageManager]["remove"] + " " + package_name;
-    CommandResult result = execute(removeCommand);
-
-    if (result.exit_code == 0) {
-        std::cout << result.stdout_str << "Package removed successfully.\n";
-        return true;
-    } else {
-        std::cerr << "Failed to remove package:\n" << result.stderr_str;
-        return false;
-    }
-}
-
-std::vector<std::string> System::searchPackages(const std::string &query) {
-    std::string searchCommand = packageManagers[activePackageManager]["search"];
-    CommandResult result = execute(searchCommand);
-
-    std::vector<std::string> results;
-
-    return results;
-}
-std::vector<std::string> System::listInstalledPackages() {
-    std::string listCommand = packageManagers[activePackageManager]["list"];
-    CommandResult result = execute(listCommand);
-
-    std::vector<std::string> results;
-
-    return results;
-}
-
-std::string System::getPackageInfo(const std::string &package_name) {
-    std::string infoCommand = packageManagers[activePackageManager]["info"];
-    CommandResult result = execute(infoCommand);
-
-    if (result.exit_code == 0) {
-        std::cout << result.stdout_str << "Package info fetched successfully.\n";
-        return result.stdout_str;
-    } else {
-        std::cerr << "Failed to fetch package info:\n" << result.stderr_str;
-        return result.stderr_str;
-    }
-}
-
-CommandResult System::execute(const std::string &command) {
+CommandResult System::execute(const std::string &command)
+{
     std::ostringstream output;
     std::ostringstream error;
 
-    try {
+    try
+    {
         bp::ipstream pipe_stream;
         bp::ipstream err_stream;
         bp::child process(command, bp::std_out > pipe_stream, bp::std_err > err_stream);
 
         std::string line;
-
-        while (pipe_stream && std::getline(pipe_stream, line)) {
-            output << line << '\n';
-        }
-
-        while (err_stream && std::getline(err_stream, line)) {
-            error << line << '\n';
-        }
+        while (std::getline(pipe_stream, line)) output << line << '\n';
+        while (std::getline(err_stream, line)) error << line << '\n';
 
         process.wait();
         return {process.exit_code(), output.str(), error.str()};
-    } catch (const std::exception &ex) {
+    }
+    catch (const std::exception &ex)
+    {
         return {-1, "", "Error executing command: " + std::string(ex.what())};
     }
+}
+
+bool System::handleCommandResult(const CommandResult &result, const std::string &command)
+{
+    if (result.exit_code != 0)
+    {
+        std::cerr << "Command: " << command << "\n"
+                  << "Error executing command:\n"
+                  << result.stdout_str << "\n"
+                  << "Exit code: " << result.exit_code << "\n"
+                  << result.stderr_str << std::endl;
+        return false;
+    }
+
+    std::cout << "Command: " << command << "\n"
+              << "Command executed successfully:\n"
+              << result.stdout_str << result.stderr_str << std::endl;
+    return true;
+}
+
+bool System::executeAndHandle(const std::string &command)
+{
+    CommandResult result = execute(command);
+    return handleCommandResult(result, command);
+}
+
+bool System::executeAndCapture(const std::string &command, std::string& output)
+{
+    CommandResult result = execute(command);
+    if (result.exit_code == 0) output = result.stdout_str;
+    return handleCommandResult(result, command);
 }
